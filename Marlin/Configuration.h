08d3732988749747cdf49e8b236f8875adc3054e
Koarl <sb0nschbob@web.de>
2021-11-27 12:54:41 +0100

    default config und tests

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -108,12 +108,6 @@
 // change serial ports to SERIAL_PORT -1 and SERIAL_PORT_2 2
 #define SERIAL_PORT -1
 
-/**
- * Select a secondary serial port on the board to use for communication with the host.
- * Currently Ethernet (-2) is only supported on Teensy 4.1 boards.
- * :[-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
- */
-#define SERIAL_PORT_2 2
 /*
  *
  * 250000 works in most cases, but you might try a lower speed if
@@ -130,7 +124,7 @@
  * Currently Ethernet (-2) is only supported on Teensy 4.1 boards.
  * :[-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-//#define SERIAL_PORT_2 -1
+#define SERIAL_PORT_2 2
 //#define BAUDRATE_2 250000   // Enable to override BAUDRATE
 
 /**
